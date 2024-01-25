e64cc48c19630ca777baefb237b5522a0877b127
exzolink <exzolink@gmail.com>
2024-01-25 06:04:47 +0300

    Looks like there's no need for locks here

--- plugin/src/main/java/it/feargames/tileculling/HiddenTileRegistry.java
+++ plugin/src/main/java/it/feargames/tileculling/HiddenTileRegistry.java
@@ -1,23 +1,17 @@
 package it.feargames.tileculling;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.block.BlockState;
 import org.bukkit.configuration.ConfigurationSection;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.logging.Logger;
-
 public class HiddenTileRegistry {
 
     private final Logger logger;
-
-    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    private final ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
-    private final ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
 
     private Material[] hiddenMaterials;
     private String[] hiddenNamespaces;
@@ -28,14 +22,17 @@ public class HiddenTileRegistry {
 
     public void load(ConfigurationSection config) {
         Set<Material> materials = new HashSet<>();
+
         for (String materialName : config.getStringList("materials")) {
             Material material = Material.getMaterial(materialName);
             if (material == null || !material.isBlock()) {
                 logger.warning("Material " + materialName + " is invalid!");
                 continue;
             }
+
             materials.add(material);
         }
+
         for (String tagName : config.getStringList("tags")) {
             Tag<?> tag;
             try {
@@ -50,7 +47,7 @@ public class HiddenTileRegistry {
                 logger.warning("Material tag " + tagName + " is not a block tag!");
                 continue;
             }
-            //noinspection unchecked
+
             Tag<Material> blockTag = (Tag<Material>) tag;
             materials.addAll(blockTag.getValues());
         }
@@ -58,42 +55,29 @@ public class HiddenTileRegistry {
     }
 
     public void load(Collection<Material> materials) {
-        try {
-            writeLock.lock();
-            hiddenMaterials = materials.toArray(new Material[0]);
-            hiddenNamespaces = materials.stream().map(material -> material.getKey().toString()).toArray(String[]::new);
-        } finally {
-            writeLock.unlock();
-        }
+        hiddenMaterials = materials.toArray(new Material[0]);
+        hiddenNamespaces = materials.stream().map(material -> material.getKey().toString()).toArray(String[]::new);
+
         logger.info("Loaded " + materials.size() + " hidden tile types");
     }
 
     public boolean shouldHide(String namespacedKey) {
-        try {
-            readLock.lock();
-            for (String current : hiddenNamespaces) {
-                if (current.equals(namespacedKey)) {
-                    return true;
-                }
+        for (String current : hiddenNamespaces) {
+            if (current.equals(namespacedKey)) {
+                return true;
             }
-            return true;
-        } finally {
-            readLock.unlock();
         }
+        return true;
+
     }
 
     public boolean shouldHide(Material material) {
-        try {
-            readLock.lock();
-            for (Material current : hiddenMaterials) {
-                if (current == material) {
-                    return true;
-                }
+        for (Material current : hiddenMaterials) {
+            if (current == material) {
+                return true;
             }
-            return true;
-        } finally {
-            readLock.unlock();
         }
+        return true;
     }
 
     public boolean shouldHide(BlockState state) {
