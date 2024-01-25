87a13d55385f72b385740c7dc64770fd8796f86b
exzolink <exzolink@gmail.com>
2024-01-25 06:04:04 +0300

    Disabled restoring onDisable, added some materials

--- plugin/src/main/java/it/feargames/tileculling/CullingPlugin.java
+++ plugin/src/main/java/it/feargames/tileculling/CullingPlugin.java
@@ -1,28 +1,25 @@
 package it.feargames.tileculling;
 
 import com.comphenix.protocol.ProtocolLibrary;
-import it.feargames.tileculling.adapter.IAdapter;
 import it.feargames.tileculling.protocol.ChunkPacketListener;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.lang.reflect.Constructor;
-import java.util.Objects;
 
 public class CullingPlugin extends JavaPlugin {
 
     private SettingsHolder settings;
     private HiddenTileRegistry hiddenTileRegistry;
 
-    private IAdapter adapter;
+    private NMSUtils nms;
     private ChunkTileVisibilityManager chunkTileVisibilityManager;
     private PlayerChunkTracker playerChunkTracker;
     private ChunkCache chunkCache;
     private VisibilityCache visibilityCache;
     private ChunkPacketListener chunkPacketListener;
 
-    private VisibilityUpdateThread visibilityUpdateThread;
+    private VisibilityUpdateTask visibilityUpdateTask;
 
     @Override
     public void onEnable() {
@@ -33,58 +30,44 @@ public class CullingPlugin extends JavaPlugin {
         hiddenTileRegistry = new HiddenTileRegistry(getLogger());
         hiddenTileRegistry.load(getConfig().getConfigurationSection("hiddenTiles"));
 
-        try {
-            String nmsVersion = getServer().getClass().getName().split("\\.")[3].substring(1);
-            getLogger().info("Loading NMS adapter: " + nmsVersion);
-            //noinspection unchecked
-            Class<? extends IAdapter> adapterClass = (Class<? extends IAdapter>) Class.forName("it.feargames.tileculling.adapter.Adapter_" + nmsVersion);
-            Objects.requireNonNull(adapterClass, "Missing NMS adapter for this MC version!");
-            Constructor<? extends IAdapter> adapterConstructor = adapterClass.getConstructor();
-            Objects.requireNonNull(adapterConstructor, "Invalid NMS adapter: missing constructor!");
-            adapter = adapterConstructor.newInstance();
-        } catch (Throwable t) {
-            throw new IllegalStateException("Unable to setup the NMS adapter!", t);
-        }
-
         playerChunkTracker = new PlayerChunkTracker(this);
         visibilityCache = new VisibilityCache();
-        chunkCache = new ChunkCache(this, hiddenTileRegistry);
-        chunkTileVisibilityManager = new ChunkTileVisibilityManager(settings, adapter, playerChunkTracker, visibilityCache, chunkCache);
+        nms = new NMSUtils();
+        chunkCache = new ChunkCache(this, hiddenTileRegistry, nms);
+        chunkTileVisibilityManager = new ChunkTileVisibilityManager(settings, nms, playerChunkTracker, visibilityCache, chunkCache);
 
         getServer().getPluginManager().registerEvents(playerChunkTracker, this);
         getServer().getPluginManager().registerEvents(chunkCache, this);
         getServer().getPluginManager().registerEvents(visibilityCache, this);
 
-        chunkPacketListener = new ChunkPacketListener(this, hiddenTileRegistry, adapter, playerChunkTracker);
+        chunkPacketListener = new ChunkPacketListener(this, hiddenTileRegistry, nms, playerChunkTracker);
         ProtocolLibrary.getProtocolManager().addPacketListener(chunkPacketListener);
 
-        visibilityUpdateThread = new VisibilityUpdateThread(chunkTileVisibilityManager);
-        visibilityUpdateThread.start();
+        visibilityUpdateTask = new VisibilityUpdateTask(chunkTileVisibilityManager);
+        visibilityUpdateTask.start(this);
     }
 
     @Override
     public void onDisable() {
-        // Remove packet listeners
         ProtocolLibrary.getProtocolManager().removePacketListeners(this);
-
-        // Stop update task
-        if (visibilityUpdateThread != null) {
-            visibilityUpdateThread.shutdown();
-        }
-
-        // Restore visibility
-        if (chunkTileVisibilityManager != null) {
-            for (Player player : getServer().getOnlinePlayers()) {
-                chunkTileVisibilityManager.restoreVisibility(player);
-            }
-        }
+        Bukkit.getScheduler().cancelTasks(this);
     }
 
-    // TODO: registry
+    public ChunkTileVisibilityManager getVisibilityManager() {
+        return chunkTileVisibilityManager;
+    }
+
+    public VisibilityUpdateTask getVisibilityUpdateTask() {
+        return visibilityUpdateTask;
+    }
+
     public static boolean isOccluding(Material material) {
         switch (material) {
+            case BARREL:
             case BARRIER:
             case SPAWNER:
+            case SUSPICIOUS_GRAVEL:
+            case SUSPICIOUS_SAND:
                 return false;
         }
         return material.isOccluding();
